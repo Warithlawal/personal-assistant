6f58a57be1acda67b460a3b083699617489700c6
Warith Lawal <79901863+Warithlawal@users.noreply.github.com>
2024-07-26 13:54:38 +0100

    Add files via upload

---
+++ personal.js
@@ -0,0 +1,27 @@
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('nav');
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+const sideMenu = document.getElementById('sidemenu');
+const overlay = document.getElementById('overlay');
+const body = document.body;
+
+function openmenu() {
+    sideMenu.style.right = "0";
+    overlay.style.display = "block";
+    body.classList.add('no-scroll');
+}
+
+function closemenu() {
+    sideMenu.style.right = "-300px";
+    overlay.style.display = "none";
+    body.classList.remove('no-scroll');
+    body.style.filter = "none";
+}
+
+overlay.addEventListener('click', closemenu);
